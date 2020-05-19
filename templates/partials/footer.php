2d27dca24303c42f1e5a07935cf2032bcddf8938
Thomas Bishop <tombishop95@gmail.com>
2020-05-19 20:40:36 +0100

    updated styling and changed footer url

--- templates/partials/footer.php
+++ templates/partials/footer.php
@@ -1,5 +1,5 @@
 <footer class="container-fluid bg-5 text-center fixed-bottom">
 	<p>Open Source Systems CW2 Submission - Thomas Bishop (21717763)</p>
-	<a href="https://github.com/tbishop95/CO551-CW2" target="_blank">Github Repository</a>
+	<a href="https://github.com/tbishop95/OSS-CW2-Submission" target="_blank">Github Repository</a>
 </footer>
 </html>
